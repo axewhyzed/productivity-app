660f724f016ce8ecc539d90f19910629f20f468d
Mihir Bambhaniya <mihir0234@outlook.com>
2025-03-21 23:13:48 +0530

    fixed layout and css changes to app

--- Pages/Site.Master.designer.vb
+++ Pages/Site.Master.designer.vb
@@ -14,15 +14,6 @@ Option Explicit On
 Partial Public Class SiteMaster
 
     '''<summary>
-    '''profileDropdown control.
-    '''</summary>
-    '''<remarks>
-    '''Auto-generated field.
-    '''To modify move field declaration from designer file to code-behind file.
-    '''</remarks>
-    Protected WithEvents profileDropdown As Global.System.Web.UI.HtmlControls.HtmlAnchor
-
-    '''<summary>
     '''MainContent control.
     '''</summary>
     '''<remarks>
